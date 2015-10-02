fbcdb65cd6f5b5bd84605cde3ab0da582445ab8c
Arif Fikri Abas <iamariffikri@hotmail.com>
2015-10-02 18:49:58 +0800

    Initial Commit

---
+++ IntelligenceDoctor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntelligenceDoctor
+//
+//  Created by Arif Fikri Abas on 10/2/15.
+//  Copyright © 2015 terato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntelligenceDoctor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IntelligenceDoctor
+//
+//  Created by Arif Fikri Abas on 10/2/15.
+//  Copyright © 2015 terato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
