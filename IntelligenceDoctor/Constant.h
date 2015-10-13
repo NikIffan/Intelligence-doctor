bb813744d6d5ec733f2ef898ae35fb8b521518cd
Arif Fikri Abas <iamariffikri@hotmail.com>
2015-10-13 08:38:14 +0800

    -Add constant class

---
+++ IntelligenceDoctor/Constant.h
@@ -0,0 +1,13 @@
+//
+//  Constant.h
+//  IntelligenceDoctor
+//
+//  Created by Arif Fikri Abas on 11/10/2015.
+//  Copyright © 2015 terato. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constant : NSObject
+
+@end
---
+++ IntelligenceDoctor/Constant.m
@@ -0,0 +1,13 @@
+//
+//  Constant.m
+//  IntelligenceDoctor
+//
+//  Created by Arif Fikri Abas on 11/10/2015.
+//  Copyright © 2015 terato. All rights reserved.
+//
+
+#import "Constant.h"
+
+@implementation Constant
+
+@end
