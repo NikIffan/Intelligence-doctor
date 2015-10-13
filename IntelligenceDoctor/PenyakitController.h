65dab02645677fe72fb0e3b1751ba400723dacdc
Arif Fikri Abas <iamariffikri@hotmail.com>
2015-10-13 08:40:24 +0800

    Penyakit View Controller

---
+++ IntelligenceDoctor/PenyakitController.h
@@ -0,0 +1,13 @@
+//
+//  PenyakitController.h
+//  IntelligenceDoctor
+//
+//  Created by Arif Fikri Abas on 11/10/2015.
+//  Copyright © 2015 terato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PenyakitController : UITableViewController
+
+@end
---
+++ IntelligenceDoctor/PenyakitController.m
@@ -0,0 +1,108 @@
+//
+//  PenyakitController.m
+//  IntelligenceDoctor
+//
+//  Created by Arif Fikri Abas on 11/10/2015.
+//  Copyright © 2015 terato. All rights reserved.
+//
+
+#import "PenyakitController.h"
+#import "SymptomControllerViewController.h"
+
+@interface PenyakitController ()
+{
+    NSArray *sicknessArray;
+}
+
+@end
+
+@implementation PenyakitController
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    self.navigationController.navigationBar.topItem.title = @"Pilih penyakit";
+    
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidLoad
+{
+    sicknessArray = [[NSArray alloc] initWithArray:[[DoktorDefine alloc] getAllSickness]];
+    
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return sicknessArray.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"sicknessCell" forIndexPath:indexPath];
+    
+    [cell.textLabel setText:[sicknessArray objectAtIndex:indexPath.row]];
+    
+    return cell;
+}
+
+/*
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate methods
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self performSegueWithIdentifier:@"showSicknessSegue" sender:self];
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if([segue.identifier isEqualToString:@"showSicknessSegue"])
+    {
+        SymptomControllerViewController *symptonVC = (SymptomControllerViewController *)segue.destinationViewController;
+        symptonVC.sicknessIndex = self.tableView.indexPathForSelectedRow.row;
+    }
+}
+
+@end
